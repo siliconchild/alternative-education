7186d5f509456f74853b1f3fdcb87cb0ada142b7
siliconchild <siliconchild.tuta.io>
2020-09-08 12:10:23 +0530

    added content to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react';
 
 const error = () => {
-  return <div></div>
-}
+  return <div>Page Not Found</div>;
+};
 
-export default error
+export default error;
