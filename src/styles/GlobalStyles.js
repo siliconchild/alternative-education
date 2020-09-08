084e46b5b637d0ac7171bb5091450dec68f03dbf
siliconchild <siliconchild.tuta.io>
2020-09-08 23:22:38 +0530

    Renamed GlobalStyles File

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle`
     :root {
         --primary-color: #5fcf80;
         --primary-color-light: #5fcf8096;
